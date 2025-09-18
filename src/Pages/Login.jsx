6e4fa8251b9be0b0e9d4cc584b258c442544de32
itzadmin001 <158562740+itzadmin001@users.noreply.github.com>
2025-09-18 13:15:10 +0530

    login update

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -126,7 +126,7 @@ function Login() {
             </div>
 
             {/* Right: Image */}
-            <div className="w-full md:w-1/2 slide-in-right">
+            <div className="sm:w-full hidden md:w-1/2 slide-in-right">
                 <img
                     src="https://images.unsplash.com/photo-1523297467724-f6758d7124c5?q=80&w=719&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                     alt="Login Illustration"
